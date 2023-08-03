df22a03c5d28f28f761f5a9ddda2dca816b8c19f
Rajitha Liyanage <rajitha1911@gmail.com>
2023-08-03 15:58:23 +0900

    update dependencies

--- src/main/java/com/across/Inventory/InventoryApplication.java
+++ src/main/java/com/across/Inventory/InventoryApplication.java
@@ -1,4 +1,4 @@
-package com.across.Inventory;
+package com.across.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
